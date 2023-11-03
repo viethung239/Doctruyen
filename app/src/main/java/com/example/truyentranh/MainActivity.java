ad98da5b6ee0ca674bcceb9324d41b8ecd444dd4
hungn <hungnguyennam6@gmail.com>
2023-11-03 15:56:03 +0700

    nguyenviethung update 11/3/2023

--- app/src/main/java/com/example/truyentranh/MainActivity.java
+++ app/src/main/java/com/example/truyentranh/MainActivity.java
@@ -2,99 +2,40 @@ package com.example.truyentranh;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
+import android.view.View;
 
-import android.widget.EditText;
-import android.widget.GridView;
-import android.widget.Toast;
+import com.example.truyentranh.databinding.ActivityMainBinding;
 
-import com.example.truyentranh.Api.ApiGetComic;
-import com.example.truyentranh.Interfaces.GetComic;
-import com.example.truyentranh.adapter.ComicAdapter;
-import com.example.truyentranh.object.Comic;
+public class MainActivity extends AppCompatActivity {
 
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-
-public class MainActivity extends AppCompatActivity implements GetComic {
-GridView gdvListComic;
-ComicAdapter adapter;
-ArrayList<Comic> ComicArrList;
-EditText SearchComic;
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        init();
-        anhXa();
-        setUp();
-        setClick();
-        new ApiGetComic(this).execute();
-
-    }
-    private void init(){
-        ComicArrList = new ArrayList<>();
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
 
-        adapter = new ComicAdapter(this,0,ComicArrList);
-    }
-    private void anhXa(){
-        gdvListComic = findViewById(R.id.gdvListComic);
-        SearchComic = findViewById(R.id.SearchComic);
-    }
-    private void setUp(){
-        gdvListComic.setAdapter(adapter);
-    }
-    private void setClick(){
-        SearchComic.addTextChangedListener(new TextWatcher() {
+        // Nhấn nút loginBtn, bắt đầu hiển thị màn hình đăng nhập
+
+        binding.loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-                String s = SearchComic .getText().toString();
-                adapter.sortComic(s);
+            public void onClick(View v) {
 
             }
         });
-    }
 
-    @Override
-    public void start() {
-        Toast.makeText(this,"Đang Lấy Về",Toast.LENGTH_SHORT).show();
-    }
+        //handle skipBtn click , bắt đầu đăng nhập mà không cần
 
-    @Override
-    public void end(String data) {
-        try {
-            ComicArrList.clear();
-            JSONArray arr = new JSONArray(data);
-            for (int i=0;i<arr.length();i++){
-                JSONObject o =arr.getJSONObject(i);
-                ComicArrList.add(new Comic(o));
+        binding.skipBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
             }
-            adapter = new ComicAdapter(this,0,ComicArrList);
-            gdvListComic.setAdapter(adapter);
-        }catch (JSONException e){
+        });
 
-        }
-    }
-
-    @Override
-    public void error() {
-        Toast.makeText(this,"Không Lấy Về Được",Toast.LENGTH_SHORT).show();
     }
 }
