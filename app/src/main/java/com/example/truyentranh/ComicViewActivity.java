dd3dc91ff1d1d226b73af79baf3de8d295dae309
Wanghuy <viquanghuy29012001@gmail.com>
2023-11-13 14:22:21 +0700

    Vi Quang Huy

---
+++ app/src/main/java/com/example/truyentranh/ComicViewActivity.java
@@ -0,0 +1,116 @@
+package com.example.truyentranh;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.truyentranh.databinding.ActivityComicViewBinding;
+import com.github.barteksc.pdfviewer.listener.OnErrorListener;
+import com.github.barteksc.pdfviewer.listener.OnPageChangeListener;
+import com.github.barteksc.pdfviewer.listener.OnPageErrorListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+public class ComicViewActivity extends AppCompatActivity {
+
+    private  String comicId;
+
+    private ActivityComicViewBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityComicViewBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Intent intent = getIntent();
+        comicId = intent.getStringExtra("comicId");
+
+
+        loadComicDetails();
+
+        // suwj kien nhan nut tro ve
+
+        binding.backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+    }
+
+    private void loadComicDetails() {
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Comics");
+        ref.child(comicId)
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        String pdfUrl = ""+snapshot.child("url").getValue();
+
+                        loadComicFromUrl(pdfUrl);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+    }
+
+    private void loadComicFromUrl(String pdfUrl) {
+
+        StorageReference ref = FirebaseStorage.getInstance().getReferenceFromUrl(pdfUrl);
+        ref.getBytes(Contrants.MAX_BYTES_PDF)
+                .addOnSuccessListener(new OnSuccessListener<byte[]>() {
+                    @Override
+                    public void onSuccess(byte[] bytes) {
+
+                        binding.pdfView.fromBytes(bytes)
+                                .swipeHorizontal(false)
+                                .onPageChange(new OnPageChangeListener() {
+                                    @Override
+                                    public void onPageChanged(int page, int pageCount) {
+
+                                        int currentPage = (page+1);
+                                        binding.toolbarappSubtiteTv.setText(currentPage +"/"+pageCount);
+
+                                    }
+                                })
+                                .onError(new OnErrorListener() {
+                                    @Override
+                                    public void onError(Throwable t) {
+                                        Toast.makeText(ComicViewActivity.this, ""+t.getMessage(), Toast.LENGTH_SHORT).show();
+                                    }
+                                })
+                                .onPageError(new OnPageErrorListener() {
+                                    @Override
+                                    public void onPageError(int page, Throwable t) {
+                                        Toast.makeText(ComicViewActivity.this, "Error on page"+page+""+t.getMessage(), Toast.LENGTH_SHORT).show();
+                                    }
+                                })
+                                .load();
+                                 binding.progressBar.setVisibility(View.GONE);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        binding.progressBar.setVisibility(View.GONE);
+                    }
+                });
+
+
+    }
+}
